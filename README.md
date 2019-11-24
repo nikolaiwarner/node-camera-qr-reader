78ae1d78de734195a69fdb7890ffc812664c34c0
Nick Warner <nickwarner@gmail.com>
2019-11-23 21:12:05 -0500

    Add options for retrying until a QR code is found

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # node-camera-qr-reader
 
 Capture and read a QR code from a camera from nodejs.
- 
+
 ## Usage
 ```
-var captureQrCode = require('node-camera-qr-reader')
+const captureQrCode = require('node-camera-qr-reader')
 
-var content = await captureQrCode()
+const opts = {
+  retry: true,                // keep checking until a QR is found, default: false
+  retryUntilTimeout: 10000    // milliseconds until giving up, default: never give up
+}
+
+const content = await captureQrCode(opts)
 ```
 
 ## Dependencies
--- index.js
+++ index.js
@@ -3,28 +3,41 @@ const jpeg = require('jpeg-js')
 const jsQR = require('jsqr')
 const nodeWebcam = require('node-webcam')
 
-function capture () {
-  var captureOpts = {
-    output: 'jpeg',
-    callbackReturn: 'buffer'
-  }
+const captureQrCode = (opts) => {
+  opts = opts || {}
   return new Promise((resolve, reject) => {
-    nodeWebcam.capture('webcam', captureOpts, ( err, buffer ) => {
-      if (err) {
-        reject(err)
-      } else {
-        const rawImage = jpeg.decode(buffer)
-        const qrCode = jsQR(rawImage.data, rawImage.width, rawImage.height)
-        if (qrCode) {
-          // console.log('Found QR code: ', qrCode.data)
-          resolve(qrCode.data)
+    if (opts.retryUntilTimeout) {
+      setTimeout(() => {
+        resolve()
+      }, opts.retryUntilTimeout)
+    }
+    const captureAndDetect = () => {
+      nodeWebcam.capture('webcam', {
+        output: 'jpeg',
+        callbackReturn: 'buffer'
+      }, (err, buffer) => {
+        if (err) {
+          reject(err)
         } else {
-          // console.log('No QR code found')
-          resolve()
+          const rawImage = jpeg.decode(buffer)
+          const qrCode = jsQR(rawImage.data, rawImage.width, rawImage.height)
+          if (qrCode) {
+            resolve(qrCode.data)
+          } else {
+            if (opts.retry) {
+              // Keep looking until QR code is found
+              setTimeout(() => {
+                captureAndDetect()
+              }, 1000)
+            } else {
+              resolve()
+            }
+          }
         }
-      }
-    })
+      })
+    }
+    captureAndDetect()
   })
 }
 
-module.exports = capture
+module.exports = captureQrCode
